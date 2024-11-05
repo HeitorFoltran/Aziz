be09f0e4eab746a8fd2192b1873292303a0f44c8
Heitor <heitormfm@gmail.com>
2024-11-04 21:39:01 -0300

    Terminei de configurar as exceções

---
+++ src/main/java/com/aziz/crud/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.aziz.crud.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityCreationException.class)
+    public ResponseEntity<String> handleEntityCreationFailure(EntityCreationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityUpdateException.class)
+    public ResponseEntity<String> handleEntityUpdateFailure(EntityUpdateException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + ex.getMessage());
+    }
+}
