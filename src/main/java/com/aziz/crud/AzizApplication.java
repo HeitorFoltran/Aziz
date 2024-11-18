43f306961ed212362cf07f83a6612a2fde6bb16d
HeitorFoltran <heitormfm@gmail.com>
2024-11-18 15:55:22 -0300

    Cofigurei as variáveis de ambiente

--- src/main/java/com/aziz/crud/AzizApplication.java
+++ src/main/java/com/aziz/crud/AzizApplication.java
@@ -1,5 +1,6 @@
 package com.aziz.crud;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzizApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.load();
+
+		System.setProperty("PASSWORD", dotenv.get("PASSWORD"));
+
+		System.setProperty("POSTGRESQL_URL", dotenv.get("POSTGRESQL_URL"));
+		System.setProperty("POSTGRESQL_USERNAME", dotenv.get("POSTGRESQL_USERNAME"));
+
+		System.setProperty("SQLSERVER_URL", dotenv.get("SQLSERVER_URL"));
+		System.setProperty("SQLSERVER_USERNAME", dotenv.get("SQLSERVER_USERNAME"));
+
 		SpringApplication.run(AzizApplication.class, args);
 	}
 
