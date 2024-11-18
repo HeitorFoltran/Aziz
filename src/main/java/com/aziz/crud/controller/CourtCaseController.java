6a14162c21e8ae920e8f5d3edb58d2560ae233ff
HeitorFoltran <heitormfm@gmail.com>
2024-11-18 12:49:47 -0300

    Patch de status do caso adicionado

--- src/main/java/com/aziz/crud/controller/CourtCaseController.java
+++ src/main/java/com/aziz/crud/controller/CourtCaseController.java
@@ -2,11 +2,7 @@ package com.aziz.crud.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.aziz.crud.model.CourtCase;
 import com.aziz.crud.service.CourtCaseService;
@@ -25,6 +21,11 @@ public class CourtCaseController extends GenericController<CourtCase, Long> {
     @PutMapping("/{id}")
     public ResponseEntity<CourtCase> updateCourtCase(@Valid @PathVariable Long id, @RequestBody CourtCase courtCaseSpecs) {
         return courtCaseService.updateEntity(id, courtCaseSpecs);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<CourtCase> updateStatus(@PathVariable Long id, @RequestParam Boolean status){
+        return courtCaseService.updateStatus(id, status);
     }
 
 }
--- src/main/java/com/aziz/crud/service/CourtCaseService.java
+++ src/main/java/com/aziz/crud/service/CourtCaseService.java
@@ -1,26 +1,40 @@
 package com.aziz.crud.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.aziz.crud.model.CourtCase;
 import com.aziz.crud.repository.CourtCaseRepository;
 
 @Service
-public class CourtCaseService extends GenericService<CourtCase, Long>{
-	
-	public CourtCaseService(CourtCaseRepository repository) {
-		super(repository);
-	}
-	
-	@Override
-	protected CourtCase performUpdate(CourtCase existingCaso, CourtCase updatedCaso) {
-		existingCaso.setDocument(updatedCaso.getDocument());
-		existingCaso.setDefender(updatedCaso.getDefender());
-		existingCaso.setDescription(updatedCaso.getDescription());
-		existingCaso.setClient(updatedCaso.getClient());
-		existingCaso.setCategory(updatedCaso.getCategory());
-		existingCaso.setStatus(updatedCaso.getStatus());
-		return existingCaso;
-	}
+public class CourtCaseService extends GenericService<CourtCase, Long> {
+
+    private final CourtCaseRepository courtCaseRepository;
+
+    public CourtCaseService(CourtCaseRepository repository, CourtCaseRepository courtCaseRepository) {
+        super(repository);
+        this.courtCaseRepository = courtCaseRepository;
+    }
+
+    @Override
+    protected CourtCase performUpdate(CourtCase existingCaso, CourtCase updatedCaso) {
+        existingCaso.setDocument(updatedCaso.getDocument());
+        existingCaso.setDefender(updatedCaso.getDefender());
+        existingCaso.setDescription(updatedCaso.getDescription());
+        existingCaso.setClient(updatedCaso.getClient());
+        existingCaso.setCategory(updatedCaso.getCategory());
+        existingCaso.setStatus(updatedCaso.getStatus());
+        return existingCaso;
+    }
+
+    public ResponseEntity<CourtCase> updateStatus(Long id, Boolean updatedStatus) {
+        CourtCase courtCase = courtCaseRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Caso não encontrado"));
+
+        courtCase.setStatus(updatedStatus);
+        courtCaseRepository.save(courtCase);
+        return ResponseEntity.ok().build();
+
+    }
 
 }
